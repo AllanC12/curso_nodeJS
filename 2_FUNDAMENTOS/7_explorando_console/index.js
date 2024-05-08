41e161412c58033e27de8fe41f01eb371d897239
AllanC12 <allandevfront@gmail.com>
2024-05-07 22:39:38 -0300

    explorando funções do console[

--- 2_FUNDAMENTOS/7_explorando_console/index.js
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -4,3 +4,14 @@ const y = 'algum texto'
 const z = [1,2,3]
 
 console.log(x, y, z)
+
+// console.count
+
+console.count(`variável x = ${x}, contagem`)
+console.count(`variável x = ${x}, contagem`)
+console.count(`variável x = ${x}, contagem`)
+console.count(`variável x = ${x}, contagem`)
+
+//outra forma de interpolação
+console.log('variável em interpolação %s', y)
+
