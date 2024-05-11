36586f0e9f45905d04b156e7ea74c5ce52dfef30
AllanC12 <allandevfront@gmail.com>
2024-05-10 21:30:57 -0300

    entendendo sobre event loop

---
+++ 2_FUNDAMENTOS/11_event_loop/index.js
@@ -0,0 +1,15 @@
+const a = () => {
+    console.log('executando a')
+}
+
+const b = () => {
+    console.log('executando b')
+}
+
+const c = () => {
+    console.log('executando c')
+}
+
+a()
+b()
+c()
