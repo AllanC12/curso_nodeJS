e69b747d21d48732a12ea6e395162f57e1dbff40
AllanC12 <allandevfront@gmail.com>
2024-05-12 22:01:10 -0300

    tratando erros no node

---
+++ 2_FUNDAMENTOS/14_erros/throw.js
@@ -0,0 +1,7 @@
+const x = 10
+
+if(!Number.isInteger(x)){
+    throw new Error('X não é um valor numérico!')
+}
+
+console.log('continuando o código...')
---
+++ 2_FUNDAMENTOS/14_erros/try_catch.js
@@ -0,0 +1,7 @@
+const x = 10
+
+try{
+    x = 23
+}catch(err){
+    console.log(`Erro: ${err}`)
+}
