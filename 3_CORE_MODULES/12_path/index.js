9b5e9d05c6b0a0e968c78cad707080b7959bf830
AllanC12 <allandevfront@gmail.com>
2024-05-22 21:09:09 -0300

    entendendo path

---
+++ 3_CORE_MODULES/12_path/index.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+const customPath = 'relatórios/allan/relatório.pdf'
+
+console.log(path.dirname(customPath))
+console.log(path.basename(customPath))
+console.log(path.extname(customPath))
