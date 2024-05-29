92c52b3ce0657c52486de0b46b812b32bd068a08
AllanC12 <allandevfront@gmail.com>
2024-05-28 22:46:06 -0300

    cadastro de conta

--- index.mjs
+++ index.mjs
@@ -5,4 +5,30 @@ import chalk from 'chalk'
 //modulos internos
 import fs from 'fs'
 
-console.log('inciamos o accounts')
+
+const operation = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'O que você deseja fazer?',
+            choices: [
+                'Criar Conta',
+                'Consultar Saldo',
+                'Depositar',
+                'Sacar'
+            ]
+        }
+    ]).then((answer) => {
+        const action = answer['action']
+        if(action === 'Criar Conta'){
+            createAccount()
+        }
+    }).catch(err => console.log(err))
+}
+operation()
+
+const createAccount = () => {
+    console.log(chalk.bgGreen.black('Parabéns por se cadastrar no nosso banco!!!'))
+    console.log(chalk.green('Defina as opções da conta a seguir'))
+}
