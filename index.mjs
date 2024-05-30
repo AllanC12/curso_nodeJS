7884dc1dbf1055ff306d8e860cc9f77e4e51f294
AllanC12 <allandevfront@gmail.com>
2024-05-30 20:05:47 -0300

    criando conta e salvando em arquivo

--- index.mjs
+++ index.mjs
@@ -31,4 +31,34 @@ operation()
 const createAccount = () => {
     console.log(chalk.bgGreen.black('Parabéns por se cadastrar no nosso banco!!!'))
     console.log(chalk.green('Defina as opções da conta a seguir'))
+
+    buildAccount()
+}
+
+
+const buildAccount = () => {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para sua conta:'
+        }
+    ]).then((answer) => {
+        const accountName = answer['accountName']
+
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(
+              chalk.bgRed.black('Esta conta ja existe , por favor tente outro nome!')
+            )
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`,'{"balance": 0}', (err) => {
+            console.log(err)
+        })
+    })
 }
