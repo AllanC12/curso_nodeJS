39642b68a90a6e3f59f67e6f87d059c22f9de605
AllanC12 <allandevfront@gmail.com>
2024-06-19 23:37:37 -0300

    iniciando tarefa 04

---
+++ 7_TEMPLATE_ENGINE/tarefa_04/index.js
@@ -0,0 +1,54 @@
+const { privateDecrypt } = require('crypto')
+const express = require('express')
+const app = express()
+
+const port = 5000
+
+const exphbs = require('express-handlebars')
+const { title } = require('process')
+
+const hbs = exphbs.create({
+    partialsDir: ['views/partials']
+})
+
+app.engine('handlebars',hbs.engine)
+app.set('view engine', 'handlebars')
+
+app.get('/products/:id',(req,res) => {
+    const id = req.params.id
+    
+    res.render('Exibindo dados do produto: ' + id)
+})
+
+app.get('/',(req,res) => {
+    const products = [
+        {
+            id: 1,
+            title: 'Teclado',
+            price: 50.00
+        },
+        {
+            id: 2,
+            title: 'mouse',
+            price: 30.00
+        },
+        {
+            id: 4,
+            title: 'Cadeira',
+            price: 300.00
+        },
+        {
+            id: 5,
+            title: 'Mesa',
+            price: 500.00
+        }
+    ]
+
+    res.render('products',{products})
+})
+
+
+
+app.listen(port , (req,res) => {
+    console.log(`app rodando na porta ${port}`)
+})
