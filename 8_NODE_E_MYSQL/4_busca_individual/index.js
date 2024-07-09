2bace740cb4a7ef6d6f02a6f664f211008b82e37
AllanC12 <allandevfront@gmail.com>
2024-07-08 21:48:53 -0300

    busca individual aplicada a livros

---
+++ 8_NODE_E_MYSQL/4_busca_individual/index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const exphbs = require("express-handlebars");
+
+const port = 5000;
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.post("/books/insert_books", (req, res) => {
+  const title = req.body.title;
+  const pages = req.body.pages;
+
+  const query = `INSERT INTO books (title,pages) VALUES ('${title}', '${pages}')`;
+  conn.query(query, (err) => {
+    console.log(err);
+  });
+
+  res.redirect("/books");
+});
+
+app.get("/books", (req, res) => {
+  const sql = `SELECT * FROM books`;
+
+  conn.query(sql, (err, data) => {
+    if (err) {
+      throw new Error(err);
+    }
+
+    const books = data;
+
+    console.log(books);
+
+    res.render("books", { books });
+  });
+});
+
+app.get("/books/:id", (req, res) => {
+  const id = req.params.id;
+
+  const sql = `SELECT * FROM books WHERE id = ${id}`;
+
+  conn.query(sql, (err, data) => {
+    if (err) {
+      throw new Error(err);
+    }
+
+    const book = data[0];
+
+    res.render("book", { book });
+  });
+});
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "database_01",
+});
+
+conn.connect((err) => {
+  if (err) {
+    throw new Error(err);
+  }
+
+  app.listen(port, () => {
+    console.log(`app rodando na porta ${port}`);
+  });
+});
