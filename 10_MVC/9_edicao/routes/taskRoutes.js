9a3bd083bd04996a98bcde7a74c9c11848ddcf51
AllanC12 <allandevfront@gmail.com>
2024-09-07 21:05:39 -0300

    buscando dados para edicao

---
+++ 10_MVC/9_edicao/routes/taskRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const TaskController = require('../controller/TaskController')
+
+router.get('/create',TaskController.createTask)
+router.post('/create',TaskController.createTaskSave)
+router.post('/remove',TaskController.removeTask)
+router.get('/edit/:id',TaskController.updateTask)
+router.get('/', TaskController.showTasks)
+
+module.exports = router
